ee2b2ff9931708bdc533d7eb558f601d198485ff
Pugmaster Flash <michael.sarr@gmail.com>
2023-11-17 15:19:54 -0800

    well its rotating..

--- src/hello.c
+++ src/hello.c
@@ -23,22 +23,28 @@
 #define SPRITE_CONFIG 0xF000
 #define SPRITE_LENGTH 10
 
-
+#define SPRITE_WIDTH 128
 
 
 
 
 void main()
 {
-    FILE * input;
-    int result;
-    uint8_t rotation =0;
-    uint16_t cos;
-    uint16_t negsin;
-    uint16_t sin;
+
+    uint8_t rotation = 0;
+    uint16_t   sprite_center_x = 100;
+    uint16_t   sprite_center_y = 100;
+
+    uint16_t   sprite_x;
+    uint16_t   sprite_y;
+    
+    uint16_t loop;
+    
+    uint8_t rotoffset = 4;
+
 
     unsigned u;
-uint8_t v;
+    uint8_t v;
 
     // Copy sprite data
     RIA.addr0 = 0;
@@ -46,7 +52,7 @@ uint8_t v;
 
     for (u = 0; u < sizeof(possum); u++)
         RIA.rw0 = possum[u];   
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[0],256);// 1.0    
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[0],256);// 256  -> 1.0, 080 -> 0.5    
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[1], 0);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[2], 0);
 
@@ -54,8 +60,8 @@ uint8_t v;
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[4], 256); // 1.0
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[5], 0);
 
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, x_pos_px, 100);
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, y_pos_px, 100);
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, x_pos_px, sprite_center_x - 90);
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, y_pos_px, sprite_center_y - 90);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, xram_sprite_ptr, 0x0000);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, log_size, 7);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, has_opacity_metadata, true);
@@ -74,19 +80,28 @@ uint8_t v;
             continue;
         v = RIA.vsync;
 
-        cos = cos_lookup_fp8_8[rotation];
-        sin = sin_lookup_fp8_8[rotation];
-        negsin = 65535- sin;
+        sprite_x = sprite_center_x + xpos_diff[rotation];
+        sprite_y = sprite_center_y + ypos_diff[rotation];
 
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[0], cos  );
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[1], negsin);
+       if (1)
+       {
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[0], cos_lookup_fp8_8[rotation]  );
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[1],  sin_lookup_fp8_8[rotation]);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[2], 0);
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[3], sin);
-        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[4], cos);
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[3], 65535 - sin_lookup_fp8_8[rotation]);
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[4], cos_lookup_fp8_8[rotation]);
         xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, transform[5], 0);
-    
+       }
+       if (true)
+        {
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, x_pos_px, sprite_x);
+        xram0_struct_set(SPRITE_CONFIG, vga_mode4_asprite_t, y_pos_px, sprite_y);
+        }
+        for(loop =0 ; loop < 20; loop ++)
+                  printf("rotation: %-2d x:%-3d   y:%-3d\n",rotation,sprite_x,sprite_y);
+
         rotation++;
-        if (rotation == 256)
+        if (rotation == 32)
         rotation = 0;
 
 
